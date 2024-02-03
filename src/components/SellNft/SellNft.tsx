bec70a6462e7761e20f344e932f688a4e7e9c4f6
worksofallwin <princeallwin199@gmail.com>
2024-02-03 14:05:40 +0530

    complete pinata integration

--- src/components/SellNft/SellNft.tsx
+++ src/components/SellNft/SellNft.tsx
@@ -7,24 +7,27 @@ import { useContract, useContractWrite, useAddress } from "@thirdweb-dev/react";
 import { ethers } from "ethers";
 import { deployedContract } from "@/constants/index";
 import { useRouter } from "next/navigation";
+import { pinFileToIPFS, pinJSONToIPFS } from "@/utils/Pinata";
 
 type formFields = {
     name: string;
     description: string;
     price: string;
+    imageUrl: string;
 };
 
 const initialFormState = {
     name: "",
     description: "",
     price: "",
+    imageUrl: "",
 };
 
 const SellNft = () => {
     const address = useAddress();
     const router = useRouter();
 
-    const { contract } = useContract(deployedContract);
+    // const { contract } = useContract(deployedContract);
 
     // const {
     //     mutateAsync: createCampaign,
@@ -33,6 +36,7 @@ const SellNft = () => {
     // } = useContractWrite(contract, "createCampaign");
 
     const [form, setForm] = useState<formFields>(initialFormState);
+    const [isImageUploading, setIsImageUploading] = useState(false);
 
     const handleFormFieldChange = (
         fieldName: keyof formFields,
@@ -43,13 +47,38 @@ const SellNft = () => {
         setForm({ ...form, [fieldName]: e.target.value });
     };
 
+    const callToBlockchain = async (imageUri: string) => {
+        // const priceInWei = ethers.utils.parseEther(form.price);
+        // setForm({ ...initialFormState });
+    };
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-
         try {
-            // console.info("contract call successs", data);
+            const metaData = {
+                name: form.name,
+                description: form.description,
+                image: `ipfs://${form.imageUrl}`,
+            };
+            const pinataMetdata = await pinJSONToIPFS(metaData);
+            callToBlockchain(pinataMetdata.data.IpfsHash);
         } catch (err) {
-            // console.error("contract call failure", err);
+            console.error("Pinning Metadata to Pinata Error", err);
+        }
+    };
+
+    const onFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        try {
+            if (e.target.files) {
+                setIsImageUploading(true);
+                console.log(e.target.files[0]);
+                const pinataResult = await pinFileToIPFS(e.target.files[0]);
+                setForm({ ...form, imageUrl: pinataResult.data.IpfsHash });
+            }
+        } catch (error) {
+            console.log("Image Upload to Pinata Error", error);
+        } finally {
+            setIsImageUploading(false);
         }
     };
 
@@ -110,21 +139,32 @@ const SellNft = () => {
                         />
                     </div>
 
-                    {/* <FormField
-                        labelName="Campaign image *"
-                        placeholder="Place image URL of your campaign"
-                        inputType="url"
-                        value={form.image}
-                        handleChange={(e) => handleFormFieldChange("image", e)}
-                    /> */}
+                    <div className="flex-1 w-full flex flex-col">
+                        <div className="font-epilogue font-medium text-[14px] leading-[22px] text-[#808191] mb-[10px]">
+                            Nft Image *
+                        </div>
+
+                        <input
+                            required
+                            onChange={onFileChange}
+                            type="file"
+                            className="py-[15px] sm:px-[25px] px-[15px] outline-none border-[1px] border-[#3a3a43] bg-transparent font-epilogue text-white text-[14px] placeholder:text-[#4b5264] rounded-[10px] sm:min-w-[300px] cursor-pointer"
+                        />
+                    </div>
 
                     <div className="flex justify-center items-center mt-[40px]">
-                        <button
-                            type="submit"
-                            className="bg-[#0041c2] px-16 rounded py-3"
-                        >
-                            Submit
-                        </button>
+                        {isImageUploading ? (
+                            <div className="bg-gray-500 px-16 rounded py-3">
+                                Image Uploading...
+                            </div>
+                        ) : (
+                            <button
+                                type="submit"
+                                className="bg-[#0041c2] px-16 rounded py-3"
+                            >
+                                Submit
+                            </button>
+                        )}
                     </div>
                 </form>
             ) : (
---
+++ src/utils/Pinata.js
@@ -0,0 +1,61 @@
+const axios = require("axios");
+const FormData = require("form-data");
+const JWT =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxOGIyZDFmMS03ZDdiLTQwZDYtOGNkYS1lODdiZjhlYWJhODIiLCJlbWFpbCI6InByaW5jZWFsbHdpbjE5OUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwicGluX3BvbGljeSI6eyJyZWdpb25zIjpbeyJpZCI6IkZSQTEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX0seyJpZCI6Ik5ZQzEiLCJkZXNpcmVkUmVwbGljYXRpb25Db3VudCI6MX1dLCJ2ZXJzaW9uIjoxfSwibWZhX2VuYWJsZWQiOmZhbHNlLCJzdGF0dXMiOiJBQ1RJVkUifSwiYXV0aGVudGljYXRpb25UeXBlIjoic2NvcGVkS2V5Iiwic2NvcGVkS2V5S2V5IjoiNGUzM2I5MmU1MTYyYzBlMTc2ODUiLCJzY29wZWRLZXlTZWNyZXQiOiI2NGRkNWJjMGY4NzAyMDY0MmYwYmZlM2E3MzgyMjNkYmM4YjE2MjU4MzgwNmVjZWM4OWFhZGUzNDI4Zjc5MmE4IiwiaWF0IjoxNzA2ODk5MTk0fQ.oAEfrvOdJkdiDm_3bzzpaCI-RjqH5EUr5e9VpGqrm3M";
+
+export const pinFileToIPFS = async (file) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const pinataMetadata = JSON.stringify({
+        name: file.name || "File Name",
+    });
+    formData.append("pinataMetadata", pinataMetadata);
+
+    const pinataOptions = JSON.stringify({
+        cidVersion: 0,
+    });
+    formData.append("pinataOptions", pinataOptions);
+
+    try {
+        const res = await axios.post(
+            "https://api.pinata.cloud/pinning/pinFileToIPFS",
+            formData,
+            {
+                maxBodyLength: "Infinity",
+                headers: {
+                    "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+                    Authorization: `Bearer ${JWT}`,
+                },
+            }
+        );
+        return res;
+    } catch (error) {
+        return error;
+    }
+};
+
+export const pinJSONToIPFS = async (metadata) => {
+    const data = JSON.stringify({
+        pinataContent: metadata,
+        pinataMetadata: {
+            name: `${metadata.name}.json`,
+        },
+    });
+
+    try {
+        const res = await axios.post(
+            "https://api.pinata.cloud/pinning/pinJSONToIPFS",
+            data,
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${JWT}`,
+                },
+            }
+        );
+        return res;
+    } catch (error) {
+        return error;
+    }
+};
